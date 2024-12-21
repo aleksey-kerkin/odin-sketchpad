549307898f2ee00ea6c147e2e6d34b7a61387b0f
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-21 21:37:52 +0500

    Add a button to set grid size of the sketchpad
    Added a button on the top of the screen that will send the user a popup
    asking for the number of squares per side for the new grid.

--- script/main.js
+++ script/main.js
@@ -1,11 +1,33 @@
 const container = document.getElementById("grid-container");
 const singleCell = document.createElement("div");
+const buttonDrawGrid = document.createElement("button");
+buttonDrawGrid.textContent = "Generate sketchpad";
+document.body.appendChild(buttonDrawGrid);
 singleCell.setAttribute("class", "grid-item");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const clonedCell = singleCell.cloneNode(true); // Clone the cell
-  clonedCell.addEventListener("mouseenter", () => {
-    clonedCell.classList.add("hovered"); // Add event listener to each cloned cell
-  });
-  container.append(clonedCell); // Append the cloned cell to the container
+function drawSketchpad(size) {
+  container.style.cssText = `width: ${size * 10}px; height: ${size * 10}px`;
+
+  for (let i = 0; i < size * size; i++) {
+    // Clone the cell
+    const clonedCell = singleCell.cloneNode(true);
+    // Add event listener to each cloned cell
+    clonedCell.addEventListener("mouseenter", () => {
+      clonedCell.classList.add("hovered");
+    });
+    // Append the cloned cell to the container
+    container.append(clonedCell);
+  }
 }
+
+buttonDrawGrid.addEventListener("click", () => {
+  let gridSize = prompt("Grid dimensions: ");
+  if (gridSize <= 100) {
+    drawSketchpad(gridSize);
+    buttonDrawGrid.remove();
+  } else if (gridSize > 100) {
+    alert("Too much grids");
+  }
+});
+
+// drawSketchpad(16);
